fba294c21df5368148aaeaed345bc9529ad7eb5a
Lee Presswood <pressedlee123@gmail.com>
2014-06-23 21:59:21 -0500

    Starting a level generator.

--- src/core/src/com/leepresswood/neondefense/entities/Field.java
+++ src/core/src/com/leepresswood/neondefense/entities/Field.java
@@ -1,18 +1,27 @@
-//
+//Game field. All towers and enemies placed here.
+//Logic: Build a grid. Let the towers be placed on the grid. Read in text file for this level to generate enemy paths.
 package com.leepresswood.neondefense.entities;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Field implements GameEntityInterface
 {
-
+	
+	
+	public Field()
+	{
+		
+	}
+	
 	@Override
 	public void update(float delta)
 	{
+		
 	}
 
 	@Override
 	public void render(float delta, SpriteBatch batch)
 	{
+		
 	}
 }
---
+++ src/core/src/com/leepresswood/neondefense/helpers/LevelGenerator.java
@@ -0,0 +1,10 @@
+//Generate the level from the passed in number
+package com.leepresswood.neondefense.helpers;
+
+public class LevelGenerator
+{
+	public LevelGenerator(int level)
+	{
+		
+	}
+}
