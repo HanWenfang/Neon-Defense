b960626a1b2bb82a75569fb82cb0b7c3c513677a
Lee Presswood <pressedlee123@gmail.com>
2014-07-10 03:22:38 -0500

    Created classes for GUI panels.

---
+++ src/core/src/com/leepresswood/neondefense/gui/Other.java
@@ -0,0 +1,15 @@
+//This is the superclass to the update and shop classes.
+package com.leepresswood.neondefense.gui;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.leepresswood.neondefense.entities.GameEntityInterface;
+
+public abstract class Other implements GameEntityInterface
+{
+
+	@Override
+	public abstract void update(float delta);
+
+	@Override
+	public abstract void render(float delta, SpriteBatch batch);
+}
