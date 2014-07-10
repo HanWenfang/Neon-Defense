bc0f4aaeecdbee5b0ed767f85db6a51d3823d400
Lee Presswood <pressedlee123@gmail.com>
2014-07-10 01:38:42 -0500

    More GUI framework.

--- src/core/src/com/leepresswood/neondefense/entities/GUI.java
+++ src/core/src/com/leepresswood/neondefense/entities/GUI.java
@@ -15,7 +15,9 @@ public class GUI implements GameEntityInterface
 	private boolean new_panel_requested;//Has a new panel been requested?
 	private boolean new_panel_shop;		//Is the new panel the shop? True. Upgrade? False.
 	private boolean open_panel_shop;		//Is the open panel the shop? True. Upgrade? False;
+	
 	private Sprite background;				//Background image
+	private Sprite other;					//This is the other panel. This will be either the update or the shop panel.
 	
 	public GUI(Assets asset_manager, Field field)
 	{//GUI will have the money amount and a quit button. Can be expanded to include tower upgrades later.
@@ -28,12 +30,33 @@ public class GUI implements GameEntityInterface
 	{//Using information passed from field, update.
 		//Change current cash value.
 		this.money += money_change;
+		
+		//Is there a new panel requested?
+		if(new_panel_requested)
+		{
+			//Reset variable to avoid infinite panel changes
+			this.new_panel_requested = false;
+			
+			//Which panel is it?
+			if(new_panel_shop) 	//Shop
+			{
+				
+			}
+			else						//Upgrade
+			{
+				
+			}
+		}
 	}
 
 	@Override
 	public void render(float delta, SpriteBatch batch)
 	{//Draw all components.
 		this.background.draw(batch);
+		
+		//Only draw the other panel if it's not null.
+		if(other != null)
+			this.other.draw(batch);
 	}
 
 	public void getUpdatesFromField(Field field)
@@ -43,14 +66,19 @@ public class GUI implements GameEntityInterface
 		//If a tower is selected, get its ID and open the update panel.
 		if(field.isTowerSelected() && this.open_panel_shop)
 		{
-			int tower_id = field.getSelectedTower();
-			this.tower_spotlight = field.getTowerFromID(tower_id);
+			this.tower_spotlight = field.getTowerFromID(field.getSelectedTower());
+			this.open_panel_shop = false;
+			this.new_panel_shop = false;
+			this.new_panel_requested = true;
 		}
 		
 		//Otherwise, if a blank panel is selected, open the shop.
 		else if(field.isShopOpen() && !this.open_panel_shop)
 		{
-			
+			this.tower_spotlight = null;
+			this.open_panel_shop = true;
+			this.new_panel_shop = true;
+			this.new_panel_requested = true;
 		}
 	}
 }
