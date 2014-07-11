c1f3befbc68e1cb010cd43635c61db0ee05a6ec9
Lee Presswood <pressedlee123@gmail.com>
2014-07-11 03:00:23 -0500

    Gave tile-finder method a better name.

--- src/core/src/com/leepresswood/neondefense/screens/ScreenTDGame.java
+++ src/core/src/com/leepresswood/neondefense/screens/ScreenTDGame.java
@@ -85,7 +85,7 @@ public class ScreenTDGame extends GameScreen implements GestureListener
 		Vector3 unprojected = this.camera.unproject(new Vector3(x, y, 0));
 		
 		//Send the location of the tap to the field and the GUI.
-		Vector2 location = this.field.isOnField(unprojected.x, unprojected.y);
+		Vector2 location = this.field.getTileCoordinatesByPoint(unprojected.x, unprojected.y);
 		if(location != null)
 		{
 			this.field.doInput(location);
