3a345ed06aad891cca62a761de0b887fc98a81e1
Lee Presswood <pressedlee123@gmail.com>
2014-07-10 21:39:18 -0500

    Working on the assets.

--- src/core/src/com/leepresswood/neondefense/generators/Assets.java
+++ src/core/src/com/leepresswood/neondefense/generators/Assets.java
@@ -7,16 +7,24 @@ public class Assets
 	//File path to tile textures
 	private static final String TILE_FILE_PATH = "tiles/";
 	private static final String TILE_UNOCCUPIED = TILE_FILE_PATH + "unoccupied.png";
-	private static final String TILE_PATH = TILE_FILE_PATH + "tint.png";
+	private static final String TILE_PATH = TILE_FILE_PATH + "tile.png";
 	
 	//GUI components
 	private static final String GUI_FILE_PATH = "gui/";
-	
-	//GUI file locations.
 	private static final String GUI_BACKGROUND = GUI_FILE_PATH + "background.png";
-
+	private static final String GUI_BUTTON = GUI_FILE_PATH + "button.png";
+	
 	//Preload all the textures for the game.
 	public Texture TEXTURE_UNOCCUPIED = new Texture(TILE_UNOCCUPIED); 
 	public Texture TEXTURE_PATH = new Texture(TILE_PATH);
 	public Texture TEXTURE_GUI_BACKGROUND = new Texture(GUI_BACKGROUND); 
+	public Texture TEXTURE_BUTTON = new Texture(GUI_BUTTON);
+	
+	public void clearTextures()
+	{//Call this at the end of the program to clear textures from video memory.
+		TEXTURE_UNOCCUPIED.dispose();
+		TEXTURE_PATH.dispose();
+		TEXTURE_GUI_BACKGROUND.dispose();
+		TEXTURE_BUTTON.dispose();
+	}
 }
