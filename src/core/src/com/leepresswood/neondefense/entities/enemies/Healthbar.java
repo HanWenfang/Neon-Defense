1d8dbbb0009a7dd505a2438f3c316a78fbd3f529
Lee Presswood <pressedlee123@gmail.com>
2014-07-14 20:47:51 -0500

    Started healthbar class.

---
+++ src/core/src/com/leepresswood/neondefense/entities/enemies/Healthbar.java
@@ -0,0 +1,12 @@
+//This is the enemy's health bar.
+//There will be a black background and a colored foreground
+//The foreground will shrink as health is lost.
+
+//Foreground might change colors as it gets lower.
+//Go from green to yellow to red?
+package com.leepresswood.neondefense.entities.enemies;
+
+public class Healthbar
+{
+	
+}
