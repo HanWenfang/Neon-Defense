c1f2ba2e8cab18f6ebba60f12947c64a82964c56
Lee Presswood <pressedlee123@gmail.com>
2014-07-19 04:20:11 -0500

    Changed variable names to be more logical.

--- src/core/src/com/leepresswood/neondefense/helpers/TimeKeeper.java
+++ src/core/src/com/leepresswood/neondefense/helpers/TimeKeeper.java
@@ -3,12 +3,12 @@ package com.leepresswood.neondefense.helpers;
 
 public class TimeKeeper
 {
-	private float timing = 0;
-	private float time_to_shoot;
+	private float current_time = 0;
+	private float time_to_meet;
 	
-	public TimeKeeper(float time_to_shoot)
+	public TimeKeeper(float time_to_meet)
 	{		
-		this.time_to_shoot = time_to_shoot;
+		this.time_to_meet = time_to_meet;
 	}
 	
 	public void update(float delta)
@@ -17,9 +17,9 @@ public class TimeKeeper
 		
 	}
 	
-	public void update(float delta, float new_time_to_shoot)
-	{//This method changes the time_to_shoot variable. You would do this during a level up.
-		this.time_to_shoot = new_time_to_shoot;
+	public void update(float delta, float new_time)
+	{//This method changes the time_to_meet variable. You would do this during a level up for a tower, for instance.
+		this.time_to_meet = new_time;
 		this.update(delta);
 	}
 }
