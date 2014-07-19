7d4191841873a752b411c4b29e5b6d6d7745e405
Lee Presswood <pressedlee123@gmail.com>
2014-07-19 04:18:50 -0500

    Moved timing elements from Bullet to TimeKeeper.

--- src/core/src/com/leepresswood/neondefense/entities/towers/projectiles/Bullet.java
+++ src/core/src/com/leepresswood/neondefense/entities/towers/projectiles/Bullet.java
@@ -6,29 +6,14 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 
 public class Bullet
-{
-	private float timing = 0;
-	private float time_to_shoot;
-	
+{	
 	private Sprite sprite;
 	
-	public Bullet(Texture t, Vector2 position, float width, float height, float time_to_shoot)
+	public Bullet(Texture t, Vector2 position, float width, float height, float direction)
 	{
 		this.sprite = new Sprite(t);
 		this.sprite.setBounds(position.x, position.y, width, height);
-		
-		this.time_to_shoot = time_to_shoot;
 	}
 	
-	public void update(float delta)
-	{//Update timing and position
-		
-		
-	}
 	
-	public void update(float delta, float new_time_to_shoot)
-	{//This method changes the time_to_shoot variable. You would do this during a level up.
-		this.time_to_shoot = new_time_to_shoot;
-		this.update(delta);
-	}
 }
--- src/core/src/com/leepresswood/neondefense/helpers/TimeKeeper.java
+++ src/core/src/com/leepresswood/neondefense/helpers/TimeKeeper.java
@@ -1,6 +1,25 @@
+//
 package com.leepresswood.neondefense.helpers;
 
 public class TimeKeeper
 {
-
+	private float timing = 0;
+	private float time_to_shoot;
+	
+	public TimeKeeper(float time_to_shoot)
+	{		
+		this.time_to_shoot = time_to_shoot;
+	}
+	
+	public void update(float delta)
+	{//Update timing
+		
+		
+	}
+	
+	public void update(float delta, float new_time_to_shoot)
+	{//This method changes the time_to_shoot variable. You would do this during a level up.
+		this.time_to_shoot = new_time_to_shoot;
+		this.update(delta);
+	}
 }
