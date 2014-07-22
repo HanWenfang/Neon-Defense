70cc19d8d365ef72685ebf2f5c79e3eb071cd6a7
Lee Presswood <pressedlee123@gmail.com>
2014-07-21 20:07:26 -0500

    Added grid to assets.

--- src/core/src/com/leepresswood/neondefense/generators/Assets.java
+++ src/core/src/com/leepresswood/neondefense/generators/Assets.java
@@ -6,10 +6,9 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 
 public class Assets
 {
-	//File path to tile textures
-	private static final String TILE_FILE_PATH = "tiles/";
-	private static final String TILE_UNOCCUPIED = TILE_FILE_PATH + "placeable.png";
-	private static final String TILE_PATH = TILE_FILE_PATH + "path.png";
+	//Background components
+	private static final String BACKGROUND_FILE_PATH = "background/";
+	private static final String BACKGROUND_GRID = BACKGROUND_FILE_PATH + "grid.png";
 	
 	//GUI components
 	private static final String GUI_FILE_PATH = "gui/";
@@ -17,11 +16,22 @@ public class Assets
 	private static final String GUI_BUTTON = GUI_FILE_PATH + "button.png";
 	private static final String TILE_GUI_SELECTED = GUI_FILE_PATH + "selected.png";
 	
+	//File path to tile textures
+	private static final String TILE_FILE_PATH = "tiles/";
+	private static final String TILE_UNOCCUPIED = TILE_FILE_PATH + "placeable.png";
+	private static final String TILE_PATH = TILE_FILE_PATH + "path.png";	
+	
+	
 	//Towers
 	private static final String TOWER_FILE_PATH = "towers/";
 	private static final String TOWER_BLASTER = TOWER_FILE_PATH + "blaster.png";
 	
+	//========================================	
+	
 	//Preload all the textures for the game.
+	//Background
+	public Texture TEXTURE_BACKGROUND_GRID = new Texture(BACKGROUND_GRID);
+	
 	//GUI
 	public Texture TEXTURE_GUI_BACKGROUND = new Texture(GUI_BACKGROUND); 
 	public Texture TEXTURE_BUTTON = new Texture(GUI_BUTTON);
@@ -34,11 +44,14 @@ public class Assets
 	//Towers
 	public Texture TEXTURE_TOWER_BLASTER = new Texture(TOWER_BLASTER);
 	
+	//========================================
+	
 	//Preload all the fonts for the game
 	public BitmapFont FONT = new BitmapFont(Gdx.files.internal("gui/white.fnt"));
 	
 	public void clear()
 	{//Call this at the end of the program to clear textures from video memory.
+		TEXTURE_BACKGROUND_GRID.dispose();
 		TEXTURE_UNOCCUPIED.dispose();
 		TEXTURE_PATH.dispose();
 		TEXTURE_GUI_BACKGROUND.dispose();
