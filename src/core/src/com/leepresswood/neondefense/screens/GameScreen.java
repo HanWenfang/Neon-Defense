7e80c62f574606016ff68dbe7106b59773d27ca7
Lee Presswood <pressedlee123@gmail.com>
2014-07-26 00:03:53 -0500

    More temporary background changes.

--- src/core/src/com/leepresswood/neondefense/screens/GameScreen.java
+++ src/core/src/com/leepresswood/neondefense/screens/GameScreen.java
@@ -20,7 +20,7 @@ public abstract class GameScreen implements Screen
 	public void render(float delta)
 	{
 		//Clear screen with black
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 	}
 	
